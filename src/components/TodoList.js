e69a0d2d9b7f501051eef96dc674f964efba50fc
developer <sk@test.com>
2023-01-31 18:27:29 +0530

    review and fix

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,67 +1,59 @@
 import React, { useEffect, useState } from "react";
-import CreateSharpIcon from "@mui/icons-material/CreateSharp";
 import { Button, TextField } from "@mui/material";
-import ClearIcon from "@mui/icons-material/Clear";
 import { v4 as uuidv4 } from "uuid";
 
 const TodoList = () => {
-    useEffect(() => {
-      const storageData=localStorage.getItem("listData")
-      if(storageData){
-        setTodoList([...JSON.parse(localStorage.getItem("listData"))]);
-      }
-    }, []);
-    const [todotask, setTodoTask] = useState("");
+
+    const [inputTask, setInputTask] = useState("");
     const [todolist, setTodoList] = useState([]);
     const [showdata, setShowData] = useState(true);
   
     const addTodoData = (e) => {
       e.preventDefault();
-      let newtask={ id: uuidv4(), data: todotask, completed: false }
+      let newtask={ id: uuidv4(), data: inputTask, completed: false }
       let updatedlist=[...todolist, newtask]
       setTodoList(updatedlist);
-        setTodoTask("");
+      setInputTask("");
       localStorage.setItem("listData", JSON.stringify(updatedlist));
     };
+    
     const handleOnDeleteItem = (e) => {
+      e.preventDefault();
       const { value } = e.target;
-      const newlist = todolist.filter((value1) => {
-        return value1.id !== value;
+      const newlist = todolist.filter((item) => {
+        return item.id !== value;
       });
       setTodoList(newlist);
-      localStorage.setItem("listData", JSON.stringify(todolist));
+      localStorage.setItem("listData", JSON.stringify(newlist));
     };
   
     const handleOnToggle = () => {
-      if(showdata===true){
-        setShowData(false);
-      }else{
-        setShowData(true);
-      }
+      setShowData(!showdata);
     };
   
     const handleOnInputChange = (e) => {
-      let listdata=todolist;
-      let {value}=e.target
+      let listdata = todolist;
+      let { value } = e.target;
           if(listdata[value].completed===true){
             listdata[value].completed=false;
           }else{
             listdata[value].completed=true;
           }
           setTodoList([...listdata])
-      }
-    console.log(todolist);
+      } 
+  
+      useEffect(() => {
+        const storageData = localStorage.getItem("listData")
+        if(storageData){
+          setTodoList([...JSON.parse(localStorage.getItem("listData"))]);
+        }
+      }, []);
+  
   return (
     <>
       <nav className="bg-gray-800 text-white py-4 px-5 text-3xl flex justify-between">
-        <div>
-          <CreateSharpIcon />
-          To Do List
-        </div>
-        <div>
-          <button className="text-xl bg-slate-200 px-4 py-1 text-black rounded-2xl">
-            UserName
-          </button>
+        <div className="flex align-center">
+          To Do List {todolist?.length > 0 ? <span className="ml-3 bg-red-500 text-white text-xs font-medium mr-2 px-2.5 rounded-full h-4">{todolist?.length}</span> : ""}
         </div>
       </nav>
       <form onSubmit={addTodoData}>
@@ -71,15 +63,15 @@ const TodoList = () => {
               placeholder="Type to add a new task"
               size="small"
               fullWidth
-              value={todotask}
-              onChange={(e) => setTodoTask(e.target.value)}
+              value={inputTask}
+              onChange={(e) => setInputTask(e.target.value)}
             />
           </div>
           <Button
             type="submit"
             color="primary"
             variant="contained"
-            disabled={todotask === "" ? true : false}
+            disabled={inputTask === "" ? true : false}
           >
             Add Task
           </Button>
@@ -91,10 +83,10 @@ const TodoList = () => {
           color="primary"
           variant="contained"
           size="small"
-          value={showdata?"Show":"Hide"}
+          value={showdata}
           onClick={handleOnToggle}
         >
-          {showdata?"Show":"Hide"}
+          {!showdata ? "Show Completed" : "Hide Completed"}
         </Button>
       </div>
       <div className="flex justify-center">
@@ -108,26 +100,24 @@ const TodoList = () => {
                   style={value.completed&&!showdata?{ display:"none" }:{display:"block"}}
                 >
                   <div className="py-2 px-2 flex ">
-                    <label className="flex w-full cursor-pointer">
+                     <label className="flex w-full cursor-pointer">
                       <input
                         type="checkbox"
                         id={value.id}
                         value={index}
-                        onClick={handleOnInputChange}
+                        onChange={handleOnInputChange}
                         checked={todolist[index].completed}
                       />
                       <p className="pl-2 py-2 w-[93%]">{value.data}</p>
-                      <div>
-                        <Button value={value.id} onClick={handleOnDeleteItem}>
-                          <ClearIcon color="error" />
-                          Delete
-                        </Button>
-                      </div>
-                    </label>
+                      </label>
+                        <button className="text-red-500 text-3xl" value={value.id} onClick={handleOnDeleteItem}>&times;</button>
                   </div>
                 </li>
               );
             })}
+
+
+          {(!todolist || todolist.length === 0) && <div className="p-4 text-center bg-gray-50">No task found.</divg>}
         </ul>
       </div>
     </>
